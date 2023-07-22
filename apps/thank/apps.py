739871b31575133ccd3c050641ac43b9420a27a8
Mahmut  Seliz <mahmutseliz-gs@hotmail.com>
2023-07-23 00:17:07 +0300

    add: start models

--- apps/thank/apps.py
+++ apps/thank/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class RecommendationConfig(AppConfig):
+class ThankConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "apps.recommendation"
+    name = "apps.thank"
